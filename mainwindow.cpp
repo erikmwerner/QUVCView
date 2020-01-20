ffc7bc9de278d1ecbd8026a35fbb1fbc3b109ec8
Erik Werner <ewerner@uci.edu>
2020-01-19 23:17:23 -0800

    now shows images

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,8 +4,9 @@
 #include <QDockWidget>
 #include <stdio.h>
 #include <QThread>
-#include <QDebug>
+#include <QTimer>
 
+MainWindow* handler;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,19 +16,38 @@ MainWindow::MainWindow(QWidget *parent)
     _settings_widget = new QDockWidget(this);
     addDockWidget(Qt::RightDockWidgetArea, _settings_widget);
 
+    m_capture_timer = new QTimer(this);
+    m_capture_timer->setTimerType(Qt::PreciseTimer);
+    connect(m_capture_timer, &QTimer::timeout,
+            this, &MainWindow::grabFrame);
+
     initUVC();
+    handler = this;
 }
 
 MainWindow::~MainWindow()
 {
+    UVCClose();
     delete ui;
 }
+
+void MainWindow::handleFrame(uvc_frame_t *bgr)
+{
+    //qDebug()<<"handling frame";
+    //QImage img((uchar*)bgr->data,bgr->width,bgr->height,bgr->step,QImage::Format_RGB888);
+
+
+    m_frame = cv::Mat(bgr->height, bgr->width,CV_8UC3, (uchar*)bgr->data, bgr->step);
+
+    ui->label->setPixmap(cvMatToQPixmap(m_frame));
+}
+
 
 /* This callback function runs once per frame. Use it to perform any
  * quick processing you need, or have it put the frame into your application's
  * input queue. If this function takes too long, you'll start losing frames. */
 void cb(uvc_frame_t *frame, void *ptr) {
-    qDebug()<<"got frame";
+    //qDebug()<<"got frame";
     uvc_frame_t *bgr;
     uvc_error_t ret;
     /* We'll convert the image from YUV/JPEG to BGR, so allocate space */
@@ -43,112 +63,116 @@ void cb(uvc_frame_t *frame, void *ptr) {
         uvc_free_frame(bgr);
         return;
     }
-    /* Call a user function:
-   *
-   * my_type *my_obj = (*my_type) ptr;
-   * my_user_function(ptr, bgr);
-   * my_other_function(ptr, bgr->data, bgr->width, bgr->height);
-   */
-    /* Call a C++ method:
-   *
-   * my_type *my_obj = (*my_type) ptr;
-   * my_obj->my_func(bgr);
-   */
-    /* Use opencv.highgui to display the image:
-   *
-   * cvImg = cvCreateImageHeader(
-   *     cvSize(bgr->width, bgr->height),
-   *     IPL_DEPTH_8U,
-   *     3);
-   *
-   * cvSetData(cvImg, bgr->data, bgr->width * 3);
-   *
-   * cvNamedWindow("Test", CV_WINDOW_AUTOSIZE);
-   * cvShowImage("Test", cvImg);
-   * cvWaitKey(10);
-   *
-   * cvReleaseImageHeader(&cvImg);
-   */
+
+    handler->handleFrame(bgr);
     uvc_free_frame(bgr);
+}
+
+void MainWindow::grabFrame()
+{
+    uvc_frame_t *frame;
+    uvc_frame_t *bgr;
+    uvc_error_t ret;
+    ret = uvc_stream_get_frame(strmhp, &frame, 1000000); //1 s timeout
+    if(ret < 0) {
+        qDebug()<<"error grabbing frame"<<ret;
+    }
+    else {
+        ret = uvc_any2bgr(frame, bgr);
+        QImage img((uchar*)bgr->data,bgr->width,bgr->height,bgr->step,QImage::Format_RGB888);
+        ui->label->setPixmap(QPixmap::fromImage(img));
+    }
 }
 
 
 
 int MainWindow::initUVC() {
-    uvc_context_t *ctx;
-    uvc_device_t *dev;
-    uvc_device_handle_t *devh;
-    uvc_stream_ctrl_t ctrl;
-    uvc_error_t res;
+
     /* Initialize a UVC service context. Libuvc will set up its own libusb
    * context. Replace NULL with a libusb_context pointer to run libuvc
    * from an existing libusb context. */
-    res = uvc_init(&ctx, NULL);
+    res = uvc_init(&ctx, nullptr);
     if (res < 0) {
         uvc_perror(res, "uvc_init");
         return res;
     }
     qDebug()<<"UVC initialized";
+
+    uvc_device_t ** list;
+    res = uvc_get_device_list(ctx, &list);
+    if(res < 0 ) {
+        qDebug()<<"error listing devices";
+    }
+
     /* Locates the first attached UVC device, stores in dev */
-    res = uvc_find_device(
-                ctx, &dev,
-                0, 0, NULL); /* filter devices: vendor_id, product_id, "serial_num" */
+    res = uvc_find_device(ctx, &dev,0, 0, nullptr); /* filter devices: vendor_id, product_id, "serial_num" */
     if (res < 0) {
+        qDebug()<<"no device found";
         uvc_perror(res, "uvc_find_device"); /* no devices found */
     }
-    else {
-        puts("Device found");
+    else
+    {
+        qDebug()<<"Device found";
+
         /* Try to open the device: requires exclusive access */
         res = uvc_open(dev, &devh);
         if (res < 0) {
             uvc_perror(res, "uvc_open"); /* unable to open device */
         }
-        else {
-            puts("Device opened");
-            /* Print out a message containing all the information that libuvc
-       * knows about the device */
+        else
+        {
+            qDebug()<<"Device opened";
+            /* Print out a message containing all the information that libuvc knows about the device */
             uvc_print_diag(devh, stderr);
+
             /* Try to negotiate a 640x480 30 fps YUYV stream profile */
+            qDebug()<<"Negotiating stream";
             res = uvc_get_stream_ctrl_format_size(
                         devh, &ctrl, /* result stored in ctrl */
                         UVC_FRAME_FORMAT_YUYV, /* YUV 422, aka YUV 4:2:2. try _COMPRESSED */
                         640, 480, 30 /* width, height, fps */
                         );
+
             /* Print out the result */
             uvc_print_stream_ctrl(&ctrl, stderr);
+
+            qDebug()<<"Done...";
             if (res < 0) {
                 uvc_perror(res, "get_mode"); /* device doesn't provide a matching stream */
             }
             else {
                 /* Start the video stream. The library will call user function cb:
-         *   cb(frame, (void*) 12345)
-         */
-
+                *  cb(frame, (void*) 12345)
+                */
+                qDebug()<<"Opening stream ctrl...";
                 res = uvc_start_streaming(devh, &ctrl, cb, (void*)12345, 0);
-                if (res < 0) {
-                    uvc_perror(res, "start_streaming"); /* unable to start stream */
-                }
-                else {
-                    puts("Streaming...");
-                    uvc_set_ae_mode(devh, 1); /* e.g., turn on auto exposure */
 
-                    this->thread()->sleep(5);
-                    //sleep(10); /* stream for 10 seconds */
-                    /* End the stream. Blocks until last callback is serviced */
-                    uvc_stop_streaming(devh);
-                    puts("Done streaming.");
-                }
+
+
+                //uvc_stream_open_ctrl(devh, &strmhp, &ctrl);
+
+                //uvc_stream_start(strmh,)
+                //qDebug()<<"starting timer with interval:"<<1000/30;
+                //m_capture_timer->start(1000/30);
+                //uvc_set_ae_mode(devh, 1); /* e.g., turn on auto exposure */
             }
-            /* Release our handle on the device */
-            uvc_close(devh);
-            puts("Device closed");
+
         }
-        /* Release the device descriptor */
-        uvc_unref_device(dev);
     }
+}
+
+void MainWindow::UVCClose()
+{
+    /* End the stream. Blocks until last callback is serviced */
+    uvc_stop_streaming(devh);
+    puts("Done streaming.");
+    /* Release our handle on the device */
+    uvc_close(devh);
+    puts("Device closed");
+    /* Release the device descriptor */
+    uvc_unref_device(dev);
     /* Close the UVC context. This closes and cleans up any existing device handles,
-   * and it closes the libusb context if one was not provided. */
+* and it closes the libusb context if one was not provided. */
     uvc_exit(ctx);
     puts("UVC exited");
-    return 0;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,9 @@
 
 #include <QMainWindow>
 #include "libuvc/libuvc.h"
+#include <opencv2/opencv.hpp>
+
+#include <QDebug>
 
 class QDockWidget;
 
@@ -22,6 +25,12 @@ public:
 
     int initUVC();
 
+    void UVCClose();
+
+    void handleFrame(uvc_frame_t *bgr);
+
+public slots:
+    void grabFrame();
 private:
     Ui::MainWindow *ui;
     QDockWidget* _settings_widget = nullptr;
@@ -31,5 +40,111 @@ private:
     uvc_device_handle_t *devh;
     uvc_stream_ctrl_t ctrl;
     uvc_error_t res;
+
+    uvc_stream_handle_t *strmhp;
+
+    QTimer* m_capture_timer = nullptr;
+    cv::Mat m_frame;
+
 };
+
+static inline QImage  cvMatToQImage( const cv::Mat &inMat )
+{
+    switch ( inMat.type() )
+    {
+    // 8-bit, 4 channel
+    case CV_8UC4:
+    {
+        QImage image( inMat.data, inMat.cols, inMat.rows, inMat.step, QImage::Format_RGB32 );
+
+        return image;
+    }
+
+        // 8-bit, 3 channel
+    case CV_8UC3:
+    {
+        QImage image( inMat.data, inMat.cols, inMat.rows, inMat.step, QImage::Format_RGB888 );
+
+        return image.rgbSwapped();
+    }
+
+        // 8-bit, 1 channel
+    case CV_8UC1:
+    {
+        static QVector<QRgb>  sColorTable;
+
+        // only create our color table once
+        if ( sColorTable.isEmpty() )
+        {
+            for ( int i = 0; i < 256; ++i )
+                sColorTable.push_back( qRgb( i, i, i ) );
+        }
+
+        QImage image( inMat.data, inMat.cols, inMat.rows, inMat.step, QImage::Format_Indexed8 );
+
+        image.setColorTable( sColorTable );
+
+        return image;
+    }
+
+    default:
+        qWarning() << "ASM::cvMatToQImage() - cv::Mat image type not handled in switch:" << inMat.type();
+        break;
+    }
+
+    return QImage();
+}
+
+static inline QPixmap cvMatToQPixmap( const cv::Mat &inMat )
+{
+    return QPixmap::fromImage( cvMatToQImage( inMat ) );
+}
+
+static inline cv::Mat QImageToCvMat( const QImage &inImage, bool inCloneImageData = true )
+{
+    switch ( inImage.format() )
+    {
+    // 8-bit, 4 channel
+    case QImage::Format_RGB32:
+    {
+        cv::Mat  mat( inImage.height(), inImage.width(), CV_8UC4, const_cast<uchar*>(inImage.bits()), inImage.bytesPerLine() );
+
+        return (inCloneImageData ? mat.clone() : mat);
+    }
+
+        // 8-bit, 3 channel
+    case QImage::Format_RGB888:
+    {
+        if ( !inCloneImageData )
+            qWarning() << "ASM::QImageToCvMat() - Conversion requires cloning since we use a temporary QImage";
+
+        QImage   swapped = inImage.rgbSwapped();
+
+        return cv::Mat( swapped.height(), swapped.width(), CV_8UC3, const_cast<uchar*>(swapped.bits()), swapped.bytesPerLine() ).clone();
+    }
+
+        // 8-bit, 1 channel
+    case QImage::Format_Indexed8:
+    {
+        cv::Mat  mat( inImage.height(), inImage.width(), CV_8UC1, const_cast<uchar*>(inImage.bits()), inImage.bytesPerLine() );
+
+        return (inCloneImageData ? mat.clone() : mat);
+    }
+
+    default:
+        qWarning() << "ASM::QImageToCvMat() - QImage format not handled in switch:" << inImage.format();
+        break;
+    }
+
+    return cv::Mat();
+}
+
+// If inPixmap exists for the lifetime of the resulting cv::Mat, pass false to inCloneImageData to share inPixmap's data
+// with the cv::Mat directly
+//    NOTE: Format_RGB888 is an exception since we need to use a local QImage and thus must clone the data regardless
+static inline cv::Mat QPixmapToCvMat( const QPixmap &inPixmap, bool inCloneImageData = true )
+{
+    return QImageToCvMat( inPixmap.toImage(), inCloneImageData );
+}
+
 #endif // MAINWINDOW_H
